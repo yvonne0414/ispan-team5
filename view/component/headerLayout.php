03b0bad7082b0f53e32761c09f2e4b38cea95ef9
yvonne Lee <77325197+yvonne0414@users.noreply.github.com>
2022-04-19 10:23:20 +0800

    新增 商品列表範例檔

---
+++ view/component/headerLayout.php
@@ -0,0 +1,6 @@
+<!-- Bootstrap CSS v5.1 -->
+<link rel="stylesheet" href="../../assets/package/bootstrap-5.1.3/dist/css/bootstrap.min.css">
+<!-- fontawesome -->
+<link rel="stylesheet" href="../../assets/package/fontawesome-free-6.0.0-web/css/all.min.css">
+<!-- style.css link -->
+<link rel="stylesheet" href="../../assets/css/style.css">
---
+++ view/component/sidemenu.php
@@ -0,0 +1,21 @@
+<!-- 側邊選單 -->
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header border-bottom">
+        <div class="d-flex text-nowrap">
+            <figure>
+                <img class="object-cover" src="" alt="Logo.jpg">
+            </figure>
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">logo name</h5>
+        </div>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <div class="list-group">
+            <a href="#" class="list-group-item my-2">商品列表</a>
+            <a href="#" class="list-group-item my-2">揪團活動</a>
+            <a href="#" class="list-group-item my-2">優惠卷</a>
+            <a href="#" class="list-group-item my-2">酒譜清單</a>
+            <a href="#" class="list-group-item my-2">訂單列表</a>
+        </div>
+    </div>
+</div>
