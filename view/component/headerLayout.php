e94e0e45e5ce775e7f8f65b8803156cf1501a487
yvonne Lee <77325197+yvonne0414@users.noreply.github.com>
2022-04-19 15:08:58 +0800

    fixed layout改成用根目錄方式

--- view/component/headerLayout.php
+++ view/component/headerLayout.php
@@ -1,6 +1,6 @@
 <!-- Bootstrap CSS v5.1 -->
-<link rel="stylesheet" href="../../assets/package/bootstrap-5.1.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="/ispan-team5/assets/package/bootstrap-5.1.3/dist/css/bootstrap.min.css">
 <!-- fontawesome -->
-<link rel="stylesheet" href="../../assets/package/fontawesome-free-6.0.0-web/css/all.min.css">
+<link rel="stylesheet" href="/ispan-team5/assets/package/fontawesome-free-6.0.0-web/css/all.min.css">
 <!-- style.css link -->
-<link rel="stylesheet" href="../../assets/css/style.css">
+<link rel="stylesheet" href="/ispan-team5/assets/css/style.css">
