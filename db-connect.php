eb04215917d785512ccf93972941f97bab185300
shu10111 <103923778+shu10111@users.noreply.github.com>
2022-04-19 10:46:18 +0800

    Update db-connect.php

--- db-connect.php
+++ db-connect.php
@@ -11,6 +11,6 @@ $conn = new mysqli($serername, $username, $password, $dbname);
 if ($conn->connect_error) {
   	die("連線失敗: " . $conn->connect_error);
 }
-// echo "連結DB成功<br>";
+//echo "連結DB成功<br>";
 
 ?>
