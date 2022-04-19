91f55f31c0eecbf85ff36067de0bf83967be3512
Ariel-mcu <80246788+Ariel-mcu@users.noreply.github.com>
2022-04-19 16:24:55 +0800

    Update group-view.php

--- view/page/group/group-view.php
+++ view/page/group/group-view.php
@@ -17,7 +17,51 @@ require("../../../db-connect.php");
   <?php require("../../component/header.php")?>
   <?php require("../../component/sidemenu.php")?>
   <div class="container">
-    <h2>官方活動</h2>
+    <h2 class="mt-3 mb-3" >官方活動</h2>
+
+    <!-- <div class="d-flex justify-content-end align-items-center">
+      <div>
+        <a class="btn btn-primary mb-3" href="">新增活動</a>
+      </div>
+    </div> -->
+
+    <table class="table table-striped">
+      <thead>
+        <tr class="table-dark">
+          <td>活動序號</td>
+          <td>活動名稱</td>
+          <td>主辦人</td>
+          <td>狀態</td>
+          <td>開團時間</td>
+          <td>結團時間</td>
+          <td class="text-end">報名</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>0000</td>
+          <td>
+            酒品賞
+          </td>
+          <td>官方人</td>
+          <td class="text-success">開團中</td>
+          <td>
+            2022/04/15
+          </td>
+          <td>
+            2022/04/30
+          </td>
+          <td class="text-end">
+            <a class="px-2" href=""><i class="fa-solid fa-pen"></i></a>
+          </td>
+        </tr>
+    
+      </tbody>
+    </table>
+
+  </div>
+  <div class="container">
+    <h2 class="mt-5 mb-3">私人活動</h2>
 
     <div class="d-flex justify-content-end align-items-center">
       <div>
@@ -44,7 +88,7 @@ require("../../../db-connect.php");
             揪品酒 high high
           </td>
           <td>Jone</td>
-          <td>開團中</td>
+          <td class="text-success">開團中</td>
           <td>
             2022/04/15
           </td>
@@ -62,7 +106,7 @@ require("../../../db-connect.php");
             我有故事你有酒嗎?
           </td>
           <td>Nabi</td>
-          <td>開團中</td>
+          <td class="text-danger">報名額滿</td>
           <td>
             2022/04/18
           </td>
