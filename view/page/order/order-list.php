6601b646a95798efecf61729ad597007b3917376
shu10111 <103923778+shu10111@users.noreply.github.com>
2022-04-22 20:15:03 +0800

    Update order-list.php

--- view/page/order/order-list.php
+++ view/page/order/order-list.php
@@ -71,14 +71,16 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
             <tbody>
                 <?php foreach ($rows as $row) : ?>
                     <tr>
-                        <td><?= $row["id"] ?></td>
+                        <td>
+                            <a href="./order-detail.php?order_id=<?=$row["id"]?>"><?=$row["id"]?></a>
+                        </td>
                         <td>
                             <a href="order-list.php?user_id=<?=$row["user_id"]?>"><?= $row["user_list_name"] ?></a>
                         </td>
                         <td><?= $row["logistics_state_cate_name"] ?></td>
                         <td><?= $row["order_time"] ?></td>
                         <td class="text-end">
-                            <a class="px-2" href=""><i class="fa-solid fa-eye"></i></a>
+                            <a class="px-2" href="./order-detail.php?id=<?=$row["user_id"]?>"><i class="fa-solid fa-eye"></i></a>
 
                         </td>
                     </tr>
