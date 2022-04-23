3fe338287fba2f2ad875f6f84eb58f6ed8e21f8e
shu10111 <pkathy111@gmail.com>
2022-04-24 01:28:57 +0800

    訂單資訊

--- view/page/order/order-detail.php
+++ view/page/order/order-detail.php
@@ -1,66 +1,66 @@
 <?php
 require("../../../db-connect.php");
-$user_id=$_GET['user_id'];
-$order_id=$_GET['order_id'];
+$user_id = $_GET['user_id'];
+$order_id = $_GET['order_id'];
 
 
 
 
 
-$usersql="SELECT * FROM user_list
+$usersql = "SELECT * FROM user_list
 WHERE id = '$user_id'
 ";
 $userresult = $conn->query($usersql);
 $userrow = $userresult->fetch_assoc();
 
-echo "userrow" ;
-echo "<br>" ;
-var_dump($userrow);
-echo "<br>" ;
+// echo "userrow";
+// echo "<br>";
+// var_dump($userrow);
+// echo "<br>";
 
 
 
-$prdsql ="SELECT prd_id, amount FROM order_detail
+$prdsql = "SELECT prd_id, amount FROM order_detail
 WHERE order_id = '$order_id'
 ";
 $prdresult = $conn->query($prdsql);
 $prdrows = $prdresult->fetch_all(MYSQLI_ASSOC);
 
-echo "prdrow" ;
-echo "<br>" ;
-var_dump($prdrows);
-echo "<br>" ;
+// echo "prdrow";
+// echo "<br>";
+// var_dump($prdrows);
+// echo "<br>";
 //echo $prdrows[0] ;
 //var_dump($prdrows[0]);
 
 // echo $prdrows[1] ["prd_id"];
 // echo $prdrows[2] ["prd_id"];
 
-foreach($prdrows as $prdrow ){
-  echo "<hr>" ;
-  var_dump($prdrow["prd_id"]); 
-}
+// foreach($prdrows as $prdrow ){
+//   echo "<hr>" ;
+//   var_dump($prdrow["prd_id"]); 
+// }
 
-$prd_id =$prdrow["prd_id"];
-var_dump($prd_id);
-
-
+// $prd_id =$prdrow["prd_id"];
+// var_dump($prd_id);
 
 
 
-$ordsql ="SELECT logistics_state, order_time FROM order_list
+
+
+$ordsql = "SELECT logistics_state, order_time FROM order_list
 WHERE  id='$order_id'
 ";
 $ordresult = $conn->query($ordsql);
 $ordrow = $ordresult->fetch_assoc();;
-echo "ordrow" ;
-echo "<br>" ;
-var_dump($ordrow);
-echo "<br>" ;
-echo "<hr>" ;
+// echo "ordrow";
+// echo "<br>";
+// var_dump($ordrow);
+// echo "<br>";
+// echo "<hr>";
 $state = $ordrow["logistics_state"];
-var_dump($state);
-echo "<hr>" ;
+// var_dump($state);
+// echo "<hr>";
 
 $stasql = "SELECT * FROM logistics_state_cate 
 WHERE id = '$state'
@@ -68,19 +68,19 @@ WHERE id = '$state'
 
 $staresult = $conn->query($stasql);
 $starow = $staresult->fetch_assoc();;
-echo "starow" ;
-echo "<br>" ;
-var_dump($starow);
-echo "<br>" ;
-$prdlistsql ="SELECT prd_num, name, price FROM prd_list
+// echo "starow";
+// echo "<br>";
+// var_dump($starow);
+// echo "<br>";
+$prdlistsql = "SELECT prd_num, name, price FROM prd_list
 
 ";
 $prdlistresult = $conn->query($prdlistsql);
 $prdlistrow = $prdlistresult->fetch_assoc();;
-echo "prdlistrow" ;
-echo "<br>" ;
-var_dump($prdlistrow);
-echo "<br>" ;
+// echo "prdlistrow";
+// echo "<br>";
+// var_dump($prdlistrow);
+// echo "<br>";
 
 
 
@@ -112,37 +112,37 @@ echo "<br>" ;
         <div class="row mb-3">
           <label for="id" class="col-sm-2 col-form-label">訂單編號</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" name="id" id="id" value="<?=$order_id?>" disabled>
+            <input type="text" class="form-control" name="id" id="id" value="<?= $order_id ?>" disabled>
           </div>
         </div>
         <div class="row mb-3">
           <label for="user" class="col-sm-2 col-form-label">訂購者</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="user" value="<?=$userrow["name"]?>" disabled>
+            <input type="text" class="form-control" id="user" value="<?= $userrow["name"] ?>" disabled>
           </div>
         </div>
         <div class="row mb-3">
           <label for="phone" class="col-sm-2 col-form-label">電話</label></label>
           <div class="col-sm-10">
-            <input type="tel" class="form-control" id="phone" value="<?=$userrow["phone"]?>" disabled>
+            <input type="tel" class="form-control" id="phone" value="<?= $userrow["phone"] ?>" disabled>
           </div>
         </div>
         <div class="row mb-3">
           <label for="address" class="col-sm-2 col-form-label">地址</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="address" value="<?=$userrow["address"]?>" disabled>
+            <input type="text" class="form-control" id="address" value="<?= $userrow["address"] ?>" disabled>
           </div>
         </div>
         <div class="row mb-3">
           <label for="ord_state" class="col-sm-2 col-form-label">訂單狀態</label>
           <div class="col-sm-10">
-            <input class="form-control" id="ord_state" value="<?=$starow["name"]?>" disabled>
+            <input class="form-control" id="ord_state" value="<?= $starow["name"] ?>" disabled>
           </div>
         </div>
         <div class="row mb-3">
           <label for="ord_time" class="col-sm-2 col-form-label">下單時間</label>
           <div class="col-sm-10">
-            <input type="datetime" class="form-control" id="ord_time" value="<?=$ordrow["order_time"]?>" disabled>
+            <input type="datetime" class="form-control" id="ord_time" value="<?= $ordrow["order_time"] ?>" disabled>
           </div>
         </div>
         <table class="table table-striped ord_table">
@@ -151,40 +151,47 @@ echo "<br>" ;
               <th scope="col">編號</th>
               <th scope="col">商品名稱</th>
               <th scope="col">單價</th>
-              <th scope="col">數量</th>
-              <th scope="col">小計</th>
+              <th class="text-end">數量</th>
+              <th class="text-end">小計</th>
             </tr>
           </thead>
           <tbody>
-          <?php  
-            
-            $prdlistsql ="SELECT prd_num, name, price FROM prd_list 
-            WHERE id =$prd_id";
-            $prdlistresult = $conn->query($prdlistsql);
-            $prdlistrows = $prdlistresult->fetch_all(MYSQLI_ASSOC);
-            
-            // echo "prdlistrow" ;
-            // echo "<br>" ;
-            // var_dump($prdlistrows);
-            // echo "<br>" ;
+            <?php
+            $total=0;
             foreach ($prdrows as $prdrow) :
-            
+              //echo "<hr>";
+              //var_dump($prdrow["prd_id"]);
+              $prd_id = $prdrow["prd_id"];
+              //var_dump($prd_id);
+              $prd_amount = $prdrow["amount"];
+              $prdlistsql = "SELECT prd_num, name, price FROM prd_list 
+            WHERE id =$prd_id";
+              $prdlistresult = $conn->query($prdlistsql);
+              $prdlistrow = $prdlistresult->fetch_assoc();
+
+              // echo "prdlistrow" ;
+              // echo "<br>" ;
+              // var_dump($prdlistrows);
+              // echo "<br>" ;
+
+
             ?>
 
-            <tr>
-              <td><?=$prdlistrow['prd_num']?></td>
-              <td><?=$prdlistrow['name']?></td>
-              <td><?=$prdlistrow['price']?></td>
-              <td></td>
-              <td></td>
-            </tr>
+              <tr>
+                <td><?= $prdlistrow['prd_num'] ?></td>
+                <td><?= $prdlistrow['name'] ?></td>
+                <td><?= '$'.$prdlistrow['price'] ?></td>
+                <td class="text-end"><?= $prd_amount?></td>
+                <td class="text-end"><?= '$'.$prdlistrow['price']*$prd_amount?></td>
+                <?php $total+=$prdlistrow['price']*$prd_amount?>
+              </tr>
             <?php endforeach; ?>
           </tbody>
           <tfoot>
             <tr>
-              <td class="text-end" colspan="5">
-                總計: 1000
-              </td>
+            <td class="text-end" colspan="5">
+              總計: $<?=$total?>
+            </td>
             </tr>
           </tfoot>
         </table>
