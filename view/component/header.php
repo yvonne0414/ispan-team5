61c0876278dfb043968523f8a574392c46845b47
yvonne Lee <77325197+yvonne0414@users.noreply.github.com>
2022-04-26 14:09:33 +0800

    header名稱 prd demo

--- view/component/header.php
+++ view/component/header.php
@@ -8,7 +8,7 @@
             <figure>
                 <img class="object-cover" src="/ispan-team5/assets/img/logo/logo.png" alt="INJOIN-Logo">
             </figure>
-            <!-- <h1 class="logo-text">INJOIN</h1> -->
+            <h1 class="logo-text m-2">後台</h1>
         </div>
         <a href="/ispan-team5/api/admin-logout.php" title='登出'><i class="fa-solid fa-right-to-bracket fa-xl text-white me-5"></i></a>
 
